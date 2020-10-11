b4b24fa2e0df25e1136721e15a6fdd057918d3da
Bhushan <bhushan1810patil@gmail.com>
2020-10-11 23:06:05 +0530

    add binary_sum in linear_sum.py

--- recursion/linear_sum.py
+++ recursion/linear_sum.py
@@ -8,6 +8,19 @@ def linearSumUtil(data: List[int], index: int) -> int:
         return 0
     return data[index - 1] + linearSumUtil(data, index - 1)
 
+def binary_sum(data: List[int]) -> int:
+    return binarySumUtil(data, 0, len(data))
+
+def binarySumUtil(data: List[int], start: int, stop: int) -> int:
+    if start >= stop:
+        return 0
+    elif start == stop - 1:
+        return data[start]
+    else:
+        mid = (start + stop) // 2
+        return binarySumUtil(data, start, mid) + binarySumUtil(data, mid, stop)
+
+
 
 if __name__ == '__main__':
     X = [3, 4, 4, 8, 10, 12, 13, 22, 27, 34, 34, 45,\
@@ -15,3 +28,4 @@ if __name__ == '__main__':
               63, 64, 65, 77, 84, 92, 97]
 
     print("Sum of sequence: ", linear_sum(X), sep='\t')
+    print("Binary sum of sequence: ", binary_sum(X), sep='\t')
