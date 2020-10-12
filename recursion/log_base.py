a2518591b0239cb2fb4ad6831f8b2759611cbb3c
Bhushan <bhushan1810patil@gmail.com>
2020-10-12 16:41:40 +0530

    add logBase function

--- recursion/log_base.py
+++ recursion/log_base.py
@@ -2,4 +2,20 @@
 # only with addition and integer division
 
 def logBase(num: int, base: int) -> int:
-    pass
+    if num == 0:
+        return -1
+    return 1 + logBase(num // base, base)
+
+if __name__ == "__main__":
+    while True:
+        x = input("Enter the number: ")
+        if x == "":
+            break
+
+        b = input("Enter the base: ")
+        if b == "":
+            break
+
+        ans = logBase(int(x), int(b))
+
+        print(f"Log of {x} with base {b} is: {ans}")
