a916f3942f3d545760d5495053eee28c422e1c33
Bhushan <bhushan1810patil@gmail.com>
2020-10-13 23:19:42 +0530

    add stack class sceleton

--- stack/stack.py
+++ stack/stack.py
@@ -5,4 +5,26 @@ class Empty(Exception):
     # attempting to access the component from empty stack
     pass
 
- 
+ class StackWithList():
+
+    def __init__(self):
+        self._stack = list()
+        self._len = 0
+
+    def __len__(self) -> int:
+        return self._len
+
+    def is_empty(self) -> bool:
+        return self._len == 0
+
+    def push(self, n: int) -> NoReturn:
+        pass
+
+    def pop(self) -> int:
+        pass
+
+    def peek(self) -> int:
+        pass
+
+
+
