5616b45cf016391ec5f28ecdf68419d41b7d2b4e
Bhushan <bhushan1810patil@gmail.com>
2020-10-14 00:20:21 +0530

    add parenthesis function

--- stack/parenthesis_checker.py
+++ stack/parenthesis_checker.py
@@ -1,7 +1,20 @@
 from stack import StackWithList
 
 def checkParenthesis(s: int) -> bool:
-    pass
+    right = ")]}"
+    left = "([{"
+    parenthesisStack = StackWithList()
+    for i in s:
+        if i in left:
+            parenthesisStack.push(i)
+
+        elif i in right:
+            if parenthesisStack.isEmpty():
+                return False
+            j = parenthesisStack.pop()
+            if left.index(j) != right.index(i):
+                return False
+    return True
 
 
 if __name__ == "__main__":
