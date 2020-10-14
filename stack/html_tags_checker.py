d72113cca79ef40b6b9a10dc858f9b5e00bb0712
Bhushan <bhushan1810patil@gmail.com>
2020-10-14 15:33:21 +0530

    create and structured html_tags_checker

---
+++ stack/html_tags_checker.py
@@ -0,0 +1,25 @@
+def html_tag_checker(raw: str) -> bool:
+    pass
+
+if __name__ == "__main__":
+    raw = """
+<body>
+<center>
+<h1> The Little Boat </h1>
+</center>
+<p> The storm tossed the little
+boat like a cheap sneaker in an
+old washing machine. The three
+drunken fishermen were used to
+such treatment, of course, but
+not the tree salesman, who even as
+a stowaway now felt that he
+had overpaid for the voyage. </p>
+<ol>
+<li> Will the salesman die? </li>
+<li> What color is the boat? </li>
+<li> And what about Naomi? </li>
+</ol>
+</body>
+"""
+    print(html_tag_checker(raw))
