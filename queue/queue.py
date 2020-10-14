3b9550fbf8baafa2949942a608cea1c7fc0b6e08
Bhushan <bhushan1810patil@gmail.com>
2020-10-14 21:45:57 +0530

    create queue.py and add queue class

---
+++ queue/queue.py
@@ -0,0 +1,16 @@
+from typing import List, NoReturn
+
+class QueueWithList:
+
+    MAXSIZE = 10
+
+    def __init__(self) -> NoReturn:
+        self._data = [None] * self.MAXSIZE
+        self._size = 0
+        self._head = -1
+        self._tail = -1
+
+    def __len__(self) -> int:
+        return self._size
+
+    
